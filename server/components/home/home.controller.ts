82d752a4bb55e0d87bbd4b2b4de5b84a5f13a681
Lucian Maran <lucian.maran@outlook.com>
2018-01-01 19:18:11 +0200

    get restricted subdomains

--- server/components/home/home.controller.ts
+++ server/components/home/home.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import stringUtil from "../../util/string";
 
 const homeController = {
 
@@ -10,16 +11,14 @@ const homeController = {
         let appSubdomain = "noApp";
 
         if (subdomains) {
-            if (subdomains.length == 0 || subdomains.length > 3) {
-                // maybe localhost, wrong app or wring subdomain(s)
-            } else {
+            if (subdomains.length > 0 && subdomains.length < 4) {
                 if (subdomains[0] === "identity") {
                     if (subdomains.length == 1) { // ["identity"]
                         envSubdomain = "prod";
                     }
                     if (subdomains.length == 2) { // ["identity", "dev"] or ["identity", app1"]
                         const s = subdomains[1];
-                        if (s == "stg" || s == "temp" || s == "temp-stg" || s == "dev") {
+                        if (isReservedSubdomain(s)) {
                             envSubdomain = s;
                         } else {
                             appSubdomain = s;
@@ -27,13 +26,13 @@ const homeController = {
                     }
                     if (subdomains.length == 3) { // ["identity", "dev", app1"]
                         const s = subdomains[1];
-                        if (s == "stg" || s == "temp" || s == "temp-stg" || s == "dev") {
+                        if (isReservedSubdomain(s)) {
                             envSubdomain = s;
                             appSubdomain = subdomains[2];
                         } // else wrong env
                     }
                 } // else localhost or wrong app
-            }
+            } // else localhost or wrong app
         }
 
 
@@ -41,5 +40,11 @@ const homeController = {
     },
 
 };
+
+function isReservedSubdomain(s) {
+    const isReserved1 = ["stg", "temp", "temp-stg", "dev"].includes(s);
+    const isReserved2 = stringUtil.endsWithValueFromList(s, [".blue", "-blue", ".green", "-green", ".blue-stg", "-blue-stg", ".green-stg", "-green-stg"]);
+    return isReserved1 || isReserved2;
+}
 
 export default homeController;
---
+++ server/util/string.test.ts
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import * as chai from "chai";
+import * as sinon from "sinon";
+
+import stringUtil from "./string";
+
+describe("String util", function () {
+
+    it("should work for endsWith", () => {
+        expect(stringUtil.endsWithValueFromList("aaa", ["aa"])).equal(true);
+        expect(stringUtil.endsWithValueFromList("aaa", ["aa", , "bb"])).equal(true);
+        expect(stringUtil.endsWithValueFromList("aaa", ["bb", "cc"])).equal(false);
+    });
+
+    // it("should work for equalsWith", () => {
+    //     expect(stringUtil.equalsWithValueFromList("aaa", ["aa"])).equal(false);
+    //     expect(stringUtil.equalsWithValueFromList("aaa", ["aa", , "aaa"])).equal(true);
+    // });
+
+});
