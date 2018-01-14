733baa65f30e471ec8008158fe82f2e631b8e73b
Lucian Maran <lucian.maran@outlook.com>
2018-01-14 20:09:55 +0200

    switch from underscore to handlebar

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,10 @@
 import * as express from "express";
 import * as path from "path";
-// import config from "./src/config/environment";
 import allRoutes from "./routes";
 
 import * as url from "url";
 import * as bodyParser from "body-parser";
 import * as randomstring from "randomstring";
-import * as cons from "consolidate";
 import * as nosql2 from "nosql";
 const nosql = nosql2.load("database.nosql");
 import * as querystring from "querystring";
@@ -16,15 +14,33 @@ import * as __string from "underscore.string";
 
 // import base64url from "base64url";
 import * as jose from "jsrsasign";
+import * as exphbs from "express-handlebars";
+// var exphbs = require('express-handlebars');
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for the token endpoint)
 
-app.engine("html", cons.underscore);
-app.set("view engine", "html");
-app.set("views", "server/files");
+app.engine(".hbs", exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "/views/layouts/"),
+    partialsDir: path.join(__dirname, "/views/partials/"),
+
+    // ensure the javascript is at the bottom of the code in express-handlebars template
+    // http://stackoverflow.com/a/25307270, http://stackoverflow.com/a/21740214
+    helpers: {
+        section: (name, options) => {
+            if (!this._sections) { this._sections = {}; }
+            this._sections[name] = options.fn(this);
+            return null;
+        },
+    },
+}));
+
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "/files/"));
 
 app.set("json spaces", 4);
 
