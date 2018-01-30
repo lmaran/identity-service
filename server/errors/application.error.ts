0bc5af4395f5214aefbb30a6870a348daf289bdb
lmaran <lucian.maran@outlook.com>
2018-01-30 17:28:25 +0200

    add some comments about hanling errors [skip ci]

--- server/errors/application.error.ts
+++ server/errors/application.error.ts
@@ -1,4 +1,4 @@
-// or use a dedicated module: https://github.com/jshttp/http-errors
+// or use a dedicated npm module (like http-errors): https://github.com/jshttp/http-errors
 // https://medium.com/learn-with-talkrise/custom-errors-with-node-express-27b91fe2d947
 
 // with this module, we can use:
@@ -9,9 +9,9 @@
     // err.code = 400;
     // throw err;
 
-// all these errors are finally caught by an errorHandler middleware
+    // all these errors are finally caught by an errorHandler middleware
 
-export class ApplicationError extends Error {
+    export class ApplicationError extends Error {
     private status: any;
     constructor(message, status) {
         super();
--- server/middlewares/errorHandler.middleware.ts
+++ server/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,32 @@
 // http://odino.org/async-slash-await-in-expressjs/
 // https://nemethgergely.com/error-handling-express-async-await/
+// http://thecodebarbarian.com/80-20-guide-to-express-error-handling
+// https://github.com/rollbar/node_rollbar/blob/master/lib/error.js
+
+
+
+// all these errors are finally caught by an errorHandler middleware
+
+// To catch all errors we can:
+// 1. run each route handler within a try/catch
+    // app.get('/users', async function(req, res, next){
+    //     try {
+    //          res.json(await db.getUsers())
+    //     } catch(err) {
+    //          next(err)
+    //     }
+    // })
+
+// 2. use a wrapper in order to call each event handler
+    // const asyncMiddleware = fn =>
+    //      (req, res, next) => {
+    //          Promise.resolve(fn(req, res, next))
+    //              .catch(err => next(err)) ;
+    //      };
+
+    // app.get('/users', asyncMiddleware(async function(req, res, next){
+    //      res.json(await db.getUsers())
+    // }))
 
 export const errorHandler = (err, req, res, next) => {
     // var newReq = reqHelper.getShortReq(req);
