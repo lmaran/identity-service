9dccf803d998654dee63a345b6ba7b3975f3b161
lmaran <lucian.maran@outlook.com>
2018-02-15 19:00:36 +0200

    caught unhandled errors [skip ci]

--- server/server.ts
+++ server/server.ts
@@ -3,16 +3,15 @@ import config from "./config";
 import app from "./app";
 import logger from "./logger";
 
-// const app = expressApp.getInstance();
 const httpServer: http.Server = http.createServer(app);
-httpServer.address();
-
 httpServer.listen(config.port);
-httpServer.on("error", onError);
-httpServer.on("close", onClose);
-httpServer.on("listening", onListening);
 
-function onError(error: any) {
+httpServer.on("listening", () => {
+    const addr = httpServer.address();
+    logger.warn(`Express server listening on port ${addr.port} in ${config.env} mode;`);
+});
+
+httpServer.on("error", (error: any) => {
     if (error.syscall !== "listen") {
         throw error;
     }
@@ -30,15 +29,51 @@ function onError(error: any) {
         default:
             throw error;
     }
-}
-
-function onListening() {
-    const addr = httpServer.address();
-    // logger.error(new Error("This is an error"));
-    // logger.log("verbose", "aaabb");
-    logger.warn(`Express server listening on port ${addr.port} in ${config.env} mode;`);
-}
-
-function onClose() {
+});
+httpServer.on("close", () => {
     logger.warn("Express server was closed");
-}
+});
+
+// https://nodejs.org/api/process.html#process_event_uncaughtexception
+// https://strongloop.com/strongblog/robust-node-applications-error-handling/
+
+// test:
+// Intentionally cause an exception, but don't catch it.
+// nonexistentFunc();
+process.on("uncaughtException", (err: Error) => {
+    logger.error(`Caught exception: ${err}\n`);
+    // https://stackoverflow.com/a/40867663
+    // The correct use of 'uncaughtException' is to perform synchronous cleanup of allocated
+    // resources (e.g. file descriptors, handles, etc) before shutting down the process.
+    process.exit(1);
+});
+
+// https://nodejs.org/api/process.html#process_event_unhandledrejection
+// http://thecodebarbarian.com/unhandled-promise-rejections-in-node.js.html
+// https://www.bennadel.com/blog/3238-logging-and-debugging-unhandled-promise-rejections-in-node-js-v1-4-1-and-later.htm
+
+// test1:
+// somePromise.then((res) => {
+//     return reportToUser(JSON.pasre(res)); // note the typo (`pasre`)
+//   }); // no `.catch` or `.then`
+// test2:
+// Promise.reject(new Error('woops')); // never attach a `catch`
+process.on("unhandledRejection", (reason: Error | any, promise: Promise<any>) => {
+    // reason - the object with which the promise was rejected (typically an Error object)
+    // promise - the Promise that was rejected
+    logger.error("Unhandled Rejection at:", promise, "reason:", reason);
+    // process.exit(1); // I think we don't have to exit in this case
+});
+
+// https://nodejs.org/api/process.html#process_event_warning
+// warning argument is an Error object (with name, message and stack)
+
+// test:
+// $ node
+// > events.defaultMaxListeners = 1;
+// > process.on("foo", () => {});
+// > process.on("foo", () => {});
+
+process.on("warning", (warning: Error) => {
+    logger.warn(warning.message, warning);
+  });
